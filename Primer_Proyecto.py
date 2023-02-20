c510dc81e7976a029cdb042495e2e1a728a6f5f6
zassory <nicolas.programador@gmail.com>
2023-02-19 22:39:20 -0300

    Strings y barras de escape

--- Primer_Proyecto.py
+++ Primer_Proyecto.py
@@ -2,3 +2,18 @@ print("Hola Mundo");
 print("Me encanta estudiar Python");
 print("Estudiar con "'Python Total'" es super divertido");
 print(550 + 5);
+print("Nicolas" + " " + "Caceres");
+print("Me llamo \"Nicolas\"");
+print("Esta es una linea\nY esta es otra linea");
+print("\tEsta es la tercera linea");
+print('This isn\'t a number');
+print('Este signo \\ es una barra invertida');
+
+print("Linea 1 \nLinea 2 \nLinea 3");
+
+print("A"+"  B"+" \tC");
+print("D"+"  E"+" \tF");
+print("G"+"  H"+" \tI");
+
+print("Barra Normal /");
+print("Barra Invertida: \\");
